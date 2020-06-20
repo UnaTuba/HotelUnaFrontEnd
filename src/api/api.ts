07d135f9ef11996e8de48c7432fa0f481b11df8f
EvaTuba <eva.tuba@gmail.com>
2020-06-21 01:14:07 +0200

    Add user logging

---
+++ src/api/api.ts
@@ -0,0 +1,80 @@
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import { ApiConfig } from '../config/api.config';
+
+export default function api(
+    path: string, 
+    method: 'get' | 'post' | 'patch' | 'delete',
+    body: any | undefined,)
+    {
+        return new Promise<ApiResponse>((resolve) =>{
+            const requestData = {
+                method: method,
+                url: path,
+                baseURL: ApiConfig.API_URL,
+                data: JSON.stringify(body),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': getToken(),
+                }, 
+            };
+
+        axios(requestData)
+        .then(res => responseHandler(res,resolve, requestData))
+        .catch(err => {
+            const response: ApiResponse = {
+                status: 'error',
+                data: err
+            };
+
+            resolve(response); });
+        });
+    }
+
+    export interface ApiResponse {
+        status: 'ok' | 'error' | 'login';
+        data: any;
+    }
+
+    function responseHandler(
+        res: AxiosResponse<any>,
+        resolve: (value?: ApiResponse) => void,
+        requestData: AxiosRequestConfig,
+        ){
+            if(res.status <200 || res.status >=300){
+                //STATUS CODE -401 - Bad Token
+                //TODO: ....
+                /*if(res.status === -401){
+                    refreshToken(requestData,resolve);
+                }*/
+
+
+                const response: ApiResponse ={
+                    status: 'error',
+                    data: res.data
+                };
+                return resolve(response);
+            }
+            if(res.data.statusCode < 0){
+                const response: ApiResponse ={
+                    status: 'ok',
+                    data: res.data
+                };
+
+                return resolve(response);
+            }
+
+            resolve(res.data);
+    }
+
+    function getToken(): string {
+        const token = localStorage.getItem('api_token');
+        return 'Berer ' + token;
+    }
+
+    export function saveToken(token: string){
+        localStorage.setItem('api_token',token);
+    }
+
+    /*function refreshToken(requestData: AxiosRequestConfig, resolve: (value?: unknown) => void): string | null{
+
+    }*/
--- src/components/UserLoginPage/UserLoginPage.tsx
+++ src/components/UserLoginPage/UserLoginPage.tsx
@@ -1,23 +1,128 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Form, Button, Col, Alert } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import api, { ApiResponse, saveToken } from '../../api/api';
+import { Redirect } from 'react-router-dom';
+
+interface UserLoginPageState {
+    username: string;
+    password: string;
+    errorMessage: string;
+    isLoggedIn: boolean;
+}
 
 export default  class UserLoginPage extends React.Component{
+    state: UserLoginPageState;
+
+    constructor(props: Readonly<{}>){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            errorMessage: '',
+            isLoggedIn: false,
+        }
+    }
+
+    private formInputChange(event: React.ChangeEvent<HTMLInputElement>){
+        const newState = Object.assign(this.state, {
+            [ event.target.id ]: event.target.value,
+        });
+
+        this.setState(newState);
+    }
+    
+    private setErrorMessage(message: string){
+        const newState = Object.assign(this.state,{
+            errorMessage: message,
+        })
+
+        this.setState(newState);
+    }
+
+    private setLoggedInState(message: boolean){
+        const newState = Object.assign(this.state,{
+            isLoggedIn: message,
+        });
+        this.setState(newState);
+    }
+
+    private doLogin() {
+        api('/auth/user/login','post', 
+                { username: this.state.username,
+                  password: this.state.password})
+        .then((res: ApiResponse)=> {
+            if (res.status === 'error'){
+                this.setErrorMessage('System error... Try again!');
+                return;
+            }
+
+            if(res.status === 'ok'){
+                if (res.data.statusCode !== undefined){
+                    let message ='';
+                    switch(res.data.statusCode){
+                        case -3001: message = "Non-existing username"; break;
+                        case -3002: message = "Wrong password"; break;
+                    }
+                    this.setErrorMessage(message);
+                    return;
+                }
+
+                saveToken(res.data.token);
+                this.setLoggedInState(true);
+                this.setErrorMessage("You are logged in!");
+                return;
+                //Preusmeriti korisnika:
+            }
+        });
+    }
+
     render(){
+        if(this.state.isLoggedIn === true){
+            return (
+                <Redirect to="/" />
+            );
+        }
+
         return (
         <Container>
+            <Col md={ { span: 6, offset: 3 } }>
             <Card>
                 <Card.Body>
                     <Card.Title>
-                    <FontAwesomeIcon icon={ faSignInAlt } /> User login
+                    <FontAwesomeIcon icon={ faSignInAlt } /> User
                     </Card.Title>
                     <Card.Text>
-                        Form for login will be here!! :D
+                        
+                        <Form>
+                            <Form.Group>
+                                <Form.Label htmlFor="username">Username</Form.Label>
+                                <Form.Control type="username" id="username"
+                                            value={ this.state.username }
+                                            onChange={ event => this.formInputChange(event as any) }/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="password">Password</Form.Label>
+                                <Form.Control type="password" id="password" 
+                                            value={ this.state.password }
+                                            onChange={ event => this.formInputChange(event as any) }/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Button variant="primary"
+                                        onClick={ () => this.doLogin()}>
+                                    Log In
+                                </Button>
+                            </Form.Group>
+                        </Form>
                     </Card.Text>
+                    <Alert variant="danger"
+                            className = {this.state.errorMessage ? '':'d-none'}>
+                        { this.state.errorMessage }
+                    </Alert>
                 </Card.Body>
             </Card>
-            
+            </Col>
         </Container>
         );
     }
