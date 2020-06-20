c2de712fdc7a68d4ed8026ab4498201f453f7ac3
EvaTuba <eva.tuba@gmail.com>
2020-06-20 16:25:42 +0200

    Add MainMenu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import {faHome,faUpload} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function App() {
   return (
     <Container>
+    
       <FontAwesomeIcon icon={ faHome } />
       &lt;3&lt;3 Welcome to Hotel Una!!! &lt;3&lt;3
       <FontAwesomeIcon icon={ faUpload } />  Upload
