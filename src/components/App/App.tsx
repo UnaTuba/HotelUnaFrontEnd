3b7b1915237387217e5f8ba4af161b563c083fa9
EvaTuba <eva.tuba@gmail.com>
2020-06-20 15:55:47 +0200

    Install bootstrap,jquery,fontawesome

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap';
+import {faHome,faUpload} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+function App() {
+  return (
+    <Container>
+      <FontAwesomeIcon icon={ faHome } />
+      &lt;3&lt;3 Welcome to Hotel Una!!! &lt;3&lt;3
+      <FontAwesomeIcon icon={ faUpload } />  Upload
+    </Container>
+  );
+}
+
+export default App;
