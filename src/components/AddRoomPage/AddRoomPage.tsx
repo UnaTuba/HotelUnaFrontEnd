b698781e347ab54613d6f40928b0acaac96d61c1
EvaTuba <eva.tuba@gmail.com>
2020-06-25 09:36:33 +0200

    Small fixs

--- src/components/AddRoomPage/AddRoomPage.tsx
+++ src/components/AddRoomPage/AddRoomPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Card, Form, Row, Alert } from 'react-bootstrap';
+import { Container, Col, Card, Form, Row, Alert, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserPlus, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
@@ -9,17 +9,19 @@ interface AddRoomPageState {
     formData: {
         numOfBeds: number;
         bedType: "single" | "double" | "bunk_bed" | "king_size";
-        balcony: boolean;
+        roomNumber: number;
+        maxCapacity: number;
+        price: number;
         orientation: "east" | "west" | "south" | "north";
         floor: number;
-        closet: boolean;
-        desk: boolean;
-        airConditioner: boolean;
-        roomNumber: number;
-        hairdryer: boolean;
-        max_capacity: number;
-        price: number;
-        wifi: boolean;
+       
+            closet: boolean;
+            balcony: boolean;
+            desk: boolean;
+            airConditioner: boolean;
+            hairdryer: boolean;
+            wifi: boolean;
+      
         
     };
 
@@ -39,17 +41,18 @@ export class AddRoomPage extends React.Component {
             formData: {
             numOfBeds: 1,
             bedType: "single",
-            balcony: false,
             orientation: "east",
             floor: 1,
-            closet: false,
-            desk: false,
-            airConditioner: false,
             roomNumber: 1,
-            hairdryer: false,
-            max_capacity: 1,
+            maxCapacity: 1,
             price: 0,
-            wifi: false,
+                balcony: false,
+                closet: false,
+                desk: false,
+                airConditioner: false,
+                hairdryer: false,
+                wifi: false,
+       
             },
         };
     }
@@ -62,6 +65,9 @@ export class AddRoomPage extends React.Component {
         const newState = Object.assign(this.state, {
             formData: newFormData,
         });
+        //let balcony = React.ChangeEvent<HTMLInputElement> document.getElementById("balcony");
+        //balcony.checked
+        
 
         this.setState(newState);
     }
@@ -96,7 +102,7 @@ export class AddRoomPage extends React.Component {
                             <Form.Group>
                                 <Form.Label htmlFor="roomNumber">Room number:</Form.Label>
                                 <Form.Control type="number" id="roomNumber"
-                                            value={ this.state.formData.floor }
+                                            value={ this.state.formData.roomNumber }
                                             onChange={ event => this.formInputChanged(event as any) } />
                             </Form.Group>
                         </Col>
@@ -140,7 +146,7 @@ export class AddRoomPage extends React.Component {
                             <Form.Group>
                                 <Form.Label htmlFor="maxCapacity">Maximal capacity:</Form.Label>
                                 <Form.Control type="number" id="maxCapacity"
-                                            value={ this.state.formData.floor }
+                                            value={ this.state.formData.maxCapacity }
                                             onChange={ event => this.formInputChanged(event as any) } />
                             </Form.Group>
                         </Col>
@@ -148,7 +154,7 @@ export class AddRoomPage extends React.Component {
                             <Form.Group>
                                 <Form.Label htmlFor="price">Price:</Form.Label>
                                 <Form.Control type="number" id="price"
-                                            value={ this.state.formData.floor }
+                                            value={ this.state.formData.price }
                                             onChange={ event => this.formInputChanged(event as any) } />
                             </Form.Group>
                         </Col>
@@ -170,29 +176,32 @@ export class AddRoomPage extends React.Component {
                         <Form.Group controlId="features">
                         
                         <Form.Check type="checkbox" label="Balcony"
-                        value="Balcony"
+                        id="balcony"
+                        value="balcony"
                         data-feature-id="balcony"
                         onChange={ (event: any) => this.formInputChanged(event as any) }
                         />
                         
                      
                         <Form.Check type="checkbox" label="Closet"
-                        value="Closet"
+                        id="closet"
+                        value="closet"
                         data-feature-id="closet"
                         onChange={ (event: any) => this.formInputChanged(event as any)} />
                         
                         <Form.Check type="checkbox" label="Air conditioner"
-                        value="Air conditioner"
+                        id="airConditioner"
+                        value="airConditioner"
                         data-feature-id="airConditioner"
                         onChange={ (event: any) => this.formInputChanged(event as any)} />
                         
                         <Form.Check type="checkbox" label="Desk"
-                        value="Desk"
+                        value="desk"
                         data-feature-id="desk"
                         onChange={ (event: any) => this.formInputChanged(event as any)} />
                         
                         <Form.Check type="checkbox" label="Hairdryer"
-                        value="Hairdryer"
+                        value="hairdryer"
                         data-feature-id="hairdryer"
                         onChange={ (event: any) => this.formInputChanged(event as any)} />
                         
@@ -205,7 +214,12 @@ export class AddRoomPage extends React.Component {
                         </Col>
                     </Row>
                     
-                    
+                    <Form.Group>
+                        <Button variant="primary"
+                                onClick={ () => this.doRegister() }>
+                            Register
+                        </Button>
+                    </Form.Group>    
                 </Form>
                 <Alert variant="danger"
                         className={ this.state.message ? '' : 'd-none' }>
@@ -225,12 +239,29 @@ export class AddRoomPage extends React.Component {
     }
 
     private doRegister() {
-        return (
+        const data = {
+            numOfBeds: this.state.formData?.numOfBeds,
+            bedType: this.state.formData?.bedType,
+            balcony: this.state.formData?.balcony,
+            orientation: this.state.formData?.orientation,
+            floor: this.state.formData?.floor,
+            closet: this.state.formData?.closet,
+            desk: this.state.formData?.desk,
+            airConditioner: this.state.formData?.airConditioner,
+            roomNumber: this.state.formData?.roomNumber,
+            hairdryer: this.state.formData?.hairdryer,
+            max_capacity: this.state.formData?.maxCapacity,
+            price: this.state.formData?.price,
+            wifi: this.state.formData?.wifi,
+            
+        };
+        console.log(data);
+        /*return (
             <p>
                 Adding a room...<br />
                 <Link to="/">Click here</Link> to to go to the home page.
             </p>
-        );
+        );*/
     }
 
 }
